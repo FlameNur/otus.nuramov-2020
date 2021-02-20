28e0500a80af2cc356849021c29ff4898995ea9a
FlameNur <nuramoff@gmail.com>
2021-02-20 16:14:28 +0300

    Версия 1.6.2 (нерабочая версия)

--- hw08-JSON-Object-Writer/src/main/java/com/nuramov/hw08_JSON_Object_Writer/Main.java
+++ hw08-JSON-Object-Writer/src/main/java/com/nuramov/hw08_JSON_Object_Writer/Main.java
@@ -1,7 +1,18 @@
 package com.nuramov.hw08_JSON_Object_Writer;
 
+import com.google.gson.Gson;
+
 public class Main {
     public static void main(String[] args) {
+        Gson gson = new Gson();
+        ClassToSerializing obj = new ClassToSerializing(1, "test", "message", 2);
+        System.out.println(obj);
 
+        String json = gson.toJson(obj);
+        System.out.println(json);
+
+        ClassToSerializing obj2 = gson.fromJson(json, ClassToSerializing.class);
+        System.out.println(obj.equals(obj2));
+        System.out.println(obj2);
     }
 }

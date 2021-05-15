1a155d44d302fd10588f1caf84b90d2edbce0625
FlameNur <nuramoff@gmail.com>
2021-05-15 12:53:48 +0300

    Версия 1.1

--- hw09-Jdbc-Template/src/main/java/com/nuramov/hw09_Jdbc_Template/JdbcTemplate.java
+++ hw09-Jdbc-Template/src/main/java/com/nuramov/hw09_Jdbc_Template/JdbcTemplate.java
@@ -1,5 +1,25 @@
 package com.nuramov.hw09_Jdbc_Template;
 
-public class JdbcTemplate {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class JdbcTemplate <T> {
+    // Просто пример проверки соединнения с БД
+    private Connection getNewConnection() throws SQLException {
+        String url = "jdbc:h2:mem:test";
+        String user = "sa";
+        String passwd = "sa";
+        return DriverManager.getConnection(url, user, passwd);
+    }
+
+    // Этот метод должен быть
+    void create(T objectData) {
+        // Что-то
+    }
+
+    // Этот метод должен быть
+    void update(T objectData) {
+        // Что-то
+    }
 }

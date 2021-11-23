768b2eb9171d447d85a62f60ba57539980a41eab
FlameNur <nuramoff@gmail.com>
2021-11-23 10:39:41 +0300

    Версия 2.4

--- hw10-Hibernate-ORM/src/main/java/com/nuramov/hw10_Hibernate_ORM/DAO/UserDAOImp.java
+++ hw10-Hibernate-ORM/src/main/java/com/nuramov/hw10_Hibernate_ORM/DAO/UserDAOImp.java
@@ -14,17 +14,16 @@ public class UserDAOImp implements UserDAO {
 
     @Override
     public User findById(long id) {
-        User user = null;
-        Transaction transaction = null;
-
+        User user;
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            // Находим пользователя (User) по id
-            user = session.get(User.class, id);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            if(transaction != null) {
-                transaction.rollback();
+            try {
+                session.beginTransaction();
+                // Находим пользователя (User) по id
+                user = session.get(User.class, id);
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                throw new RuntimeException(e);
             }
         }
         return user;
@@ -32,49 +31,49 @@ public class UserDAOImp implements UserDAO {
 
     @Override
     public void save(User user) {
-        Transaction transaction = null;
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            // Сохраняем пользователя (User) в БД
-            session.save(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            if(transaction != null) {
-                transaction.rollback();
+            try {
+                session.beginTransaction();
+                // Сохраняем пользователя (User) в БД
+                session.save(user);
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                throw new RuntimeException(e);
             }
         }
     }
 
     @Override
     public void update(User user) {
-        Transaction transaction = null;
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            // Обновляем данные пользователя (User) в БД
-            session.update(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            if(transaction != null) {
-                transaction.rollback();
+            try {
+                session.beginTransaction();
+                // Обновляем данные пользователя (User) в БД
+                session.update(user);
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                throw new RuntimeException(e);
             }
         }
     }
 
     @Override
     public void delete(User user) {
-        Transaction transaction = null;
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            // Сначала присваиваем полю phone значение null, чтобы в случае удаления пользователя (User) из БД
-            // не был удален Phone, на котрого ссылаются другие пользователи
-            user.setPhone(null);
-            session.update(user);
-            // Удаляем пользователя (User) из БД
-            session.delete(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            if(transaction != null) {
-                transaction.rollback();
+            try {
+                session.beginTransaction();
+                // Сначала присваиваем полю phone значение null, чтобы в случае удаления пользователя (User) из БД
+                // не был удален Phone, на котрого ссылаются другие пользователи
+                user.setPhone(null);
+                session.update(user);
+                // Удаляем пользователя (User) из БД
+                session.delete(user);
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                session.getTransaction().rollback();
+                throw new RuntimeException(e);
             }
         }
     }

f7dd4a5b69a515c7575dd03653fa5ba06add3324
nuramoff <nuramoff@gmail.com>
2022-03-21 17:04:23 +0300

    Версия 1.12

--- hw12-Web-Server/src/main/java/com/nuramov/hw12_Web_Server/servlets/UserSave.java
+++ hw12-Web-Server/src/main/java/com/nuramov/hw12_Web_Server/servlets/UserSave.java
@@ -1,6 +1,9 @@
 package com.nuramov.hw12_Web_Server.servlets;
 
-import jakarta.servlet.RequestDispatcher;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.Version;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,17 +11,36 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet("/userSave")
 public class UserSave extends HttpServlet {
 
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        request.getRequestDispatcher("/staticResources/index.html").forward(request, response);
+        Configuration cfg = new Configuration(new Version("2.3.31"));
 
-        getServletContext().getRequestDispatcher("/staticResources/index.html").forward(request, response);
+        cfg.setClassForTemplateLoading(UserSave.class, "/");
+        cfg.setDefaultEncoding("UTF-8");
 
-        response.sendRedirect(request.getContextPath() + "/staticResources/index.html");
+        Template template = cfg.getTemplate("test.ftl");
+
+        Map<String, Object> templateData = new HashMap<>();
+        templateData.put("msg", "Today is a beautiful day");
+
+        try (StringWriter out = new StringWriter()) {
+
+            template.process(templateData, out);
+            System.out.println(out.getBuffer().toString());
+
+            out.flush();
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
